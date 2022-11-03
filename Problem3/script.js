5bfc5705528115c6b6882937d154d0e436a2330e
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2022-11-03 16:45:38 +0000

    Setup for problem 3

---
+++ Problem3/script.js
@@ -0,0 +1,32 @@
+
+document.getElementById("submitUserName").addEventListener("click", function() {
+
+	userName = document.getElementById("searchUserName").value
+	fetch('https://api.github.com/users/'+userName).then((response) => response.json())
+	.then(function(data) {
+		console.log("User")
+		console.log(data)
+
+	makeTable(data)
+
+	});
+});
+
+function makeTable(user) {
+
+	document.getElementById("userInfo").innerHTML=`
+
+	<div>
+		<div class="block">
+			<h2> User Profile </h2>
+
+		</div>
+
+		<div class="block">
+			<h2> User Repos </h2>
+		</div>
+	</div>
+
+`
+
+}
